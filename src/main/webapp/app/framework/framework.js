787782d46b2cbebee13fbd5b9cdc9c8ce19f5299
zpygithub <873333508@qq.com>
2017-09-29 16:10:57 +0800

    Update framework.js

--- src/main/webapp/app/framework/framework.js
+++ src/main/webapp/app/framework/framework.js
@@ -1,23 +1,30 @@
-define(["app/services/httpService",
+define(["angular",
+        "ui-router",
+        "app/services/httpService",
+        "app/services/maskService",
         "app/framework/controllers/menusCtrl",
+        "app/framework/controllers/serviceCtrl",
         "app/framework/configures/frameworkConfig"],
-    function (http, menusCtrl, frameworkConfig) {
+    function (angular, router, http, mask, menusCtrl, serviceCtrl, frameworkConfig) {
         "use strict";
         var dependency = [
+            "ng",  
+            "ui.router",
             frameworkConfig.name,
         ];
         console.log("framework1");
 
         var framework = angular.module("framework", dependency);
 
-        framework.controller("menusCtrl", menusCtrl);
-
         framework.service("camel", http);
-        console.log("framework2");
-        framework.config(["$controllerProvider", "$compileProvider", function ($controllerProvider, $compileProvider) {
-            framework.controllerProvider = $controllerProvider;
-            framework.compileProvider = $compileProvider;
-        }]);
+        framework.service("mask", mask);
+        framework.controller("menusCtrl", menusCtrl);
+        framework.controller("serviceCtrl", serviceCtrl);
+
+//         framework.config(["$controllerProvider", "$compileProvider", function ($controllerProvider, $compileProvider) {
+//             framework.controllerProvider = $controllerProvider;
+//             framework.compileProvider = $compileProvider;
+//         }]);
 
         return framework;
     })
