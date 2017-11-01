4d8474c17bc4ea0d1e774426f9536ab7430fdb62
zpygithub <873333508@qq.com>
2017-11-01 15:56:08 +0800

    Update register.js

--- src/main/webapp/app/src/register/controllers/register.js
+++ src/main/webapp/app/src/register/controllers/register.js
@@ -2,45 +2,45 @@ $(document).ready(function () {
     $("#register").bind("click", function () {
         var account = $("#account").val();
         if ("" == account) {
-            alert("账号不能为空");
+            alert(getLocaleMsg("accountCanNotBeEmpty"));
             return false;
         }
         var isAccount = /[0-9a-zA-Z]{6,20}/;
         if (!isAccount.test(account)) {
-            alert("账号格式为6至20位数字或字母");
+            alert(getLocaleMsg("accountFormatError"));
             return false;
         }
 
         var nickname = $("#nickname").val();
         if ("" == nickname) {
-            alert("昵称不能为空");
+            alert(getLocaleMsg("nicknameCanNotBeEmpty"));
             return false;
         }
         var isNickname = /.{2,8}/;
         if (!isNickname.test(nickname)) {
-            alert("昵称格式位2至8位任意字符");
+            alert(getLocaleMsg("nicknameFormatError"));
             return false;
         }
 
         var password = $("#password").val();
         if ("" == password) {
-            alert("密码不能为空");
+            alert(getLocaleMsg("passwordCanNotBeEmpty"));
             return false;
         }
         var isPassword = /[0-9a-zA-Z`~!@#$^&*()-_=+{}\[\];:'\",.<>/?]{6,20}/;
         if (!isPassword.test(password)) {
-            alert("密码格式为6至20位数字、字母或特殊符号");
+            alert(getLocaleMsg("passwordFormatError"));
             return false;
         }
 
         var confirmPassword = $("#confirmPassword").val();
         if ("" == confirmPassword) {
-            alert("确认密码不能为空");
+            alert(getLocaleMsg("confirmPasswordCanNotBeEmpty"));
             return false;
         }
 
         if (password != confirmPassword) {
-            alert("密码必须一致");
+            alert(getLocaleMsg("passwordMustBeConsistent"));
             return false;
         }
 
@@ -58,20 +58,17 @@ $(document).ready(function () {
             data: data,
             success: function (data) {
                 if (data.code == "00000") {
-                    alert("注册成功！")
+                    alert(getLocaleMsg("registrationSuccess"));
                     window.location.href = "login.html";
                     return;
-                } else if (data.code == "00003") {
-                    alert("全都需要填！")
-                    return;
                 } else if (data.code == "00004") {
-                    alert("密码必须一致！")
+                    alert(getLocaleMsg("passwordMustBeConsistent！"));
                     return;
                 } else if (data.code == "00005") {
-                    alert("账号已存在！")
+                    alert(getLocaleMsg("accountAlreadyExists"));
                     return;
                 } else if (data.code == "00006") {
-                    alert("昵称已存在！")
+                    alert(getLocaleMsg("nicknameAlreadyExists"));
                     return;
                 }
             },
@@ -82,3 +79,7 @@ $(document).ready(function () {
         window.location.href = "login.html";
     });
 });
+
+function getLocaleMsg(key) {
+    return msg[key] == undefined ? key : msg[key];
+}
