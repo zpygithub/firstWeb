8002b0c56f00927f00c31d0f4ff12d5fef730695
zpygithub <873333508@qq.com>
2017-11-02 17:25:34 +0800

    Update login_zh_CN.js

--- src/main/webapp/i18n/zh/login_zh_CN.js
+++ src/main/webapp/i18n/zh/login_zh_CN.js
@@ -3,7 +3,7 @@ var msg = {
     "accountOrPasswordError": "账号或密码错误。",
     "accountLocked": "账号已被锁定。",
     "accountNotExist": "账号不存在。",
-    "requiredUsername": "请输入用户名。",
+    "requiredUsername": "请输入账号。",
     "requiredPassword": "请输入密码。",
     //register
     "accountCanNotBeEmpty": "账号不能为空。",
