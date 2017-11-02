cad96a8720139ac9e77ee8f9b840d652aa7b60d2
zpygithub <873333508@qq.com>
2017-11-02 17:27:28 +0800

    Update register.js

--- src/main/webapp/app/src/register/controllers/register.js
+++ src/main/webapp/app/src/register/controllers/register.js
@@ -2,45 +2,45 @@ $(document).ready(function () {
     $("#register").bind("click", function () {
         var account = $("#account").val();
         if ("" == account) {
-            alert(getLocaleMsg("accountCanNotBeEmpty"));
+            Lobibox.notify("error", {msg: getLocaleMsg("accountCanNotBeEmpty")});
             return false;
         }
         var isAccount = /[0-9a-zA-Z]{6,20}/;
         if (!isAccount.test(account)) {
-            alert(getLocaleMsg("accountFormatError"));
+            Lobibox.notify("error", {msg: getLocaleMsg("accountFormatError")});
             return false;
         }
 
         var nickname = $("#nickname").val();
         if ("" == nickname) {
-            alert(getLocaleMsg("nicknameCanNotBeEmpty"));
+            Lobibox.notify("error", {msg: getLocaleMsg("nicknameCanNotBeEmpty")});
             return false;
         }
         var isNickname = /.{2,8}/;
         if (!isNickname.test(nickname)) {
-            alert(getLocaleMsg("nicknameFormatError"));
+            Lobibox.notify("error", {msg: getLocaleMsg("nicknameFormatError")});
             return false;
         }
 
         var password = $("#password").val();
         if ("" == password) {
-            alert(getLocaleMsg("passwordCanNotBeEmpty"));
+            Lobibox.notify("error", {msg: getLocaleMsg("passwordCanNotBeEmpty")});
             return false;
         }
         var isPassword = /[0-9a-zA-Z`~!@#$^&*()-_=+{}\[\];:'\",.<>/?]{6,20}/;
         if (!isPassword.test(password)) {
-            alert(getLocaleMsg("passwordFormatError"));
+            Lobibox.notify("error", {msg: getLocaleMsg("passwordFormatError")});
             return false;
         }
 
         var confirmPassword = $("#confirmPassword").val();
         if ("" == confirmPassword) {
-            alert(getLocaleMsg("confirmPasswordCanNotBeEmpty"));
+            Lobibox.notify("error", {msg: getLocaleMsg("confirmPasswordCanNotBeEmpty")});
             return false;
         }
 
         if (password != confirmPassword) {
-            alert(getLocaleMsg("passwordMustBeConsistent"));
+            Lobibox.notify("error", {msg: getLocaleMsg("passwordMustBeConsistent")});
             return false;
         }
 
@@ -51,25 +51,21 @@ $(document).ready(function () {
             "confirmPasswd": confirmPassword,
         };
         $.ajax({
-            type: 'put',
-            url: 'register/mainRegister',
-            dataType: 'json',
+            type: "put",
+            url: "register/mainRegister",
+            dataType: "json",
             async: false,
             data: data,
             success: function (data) {
                 if (data.code == "00000") {
-                    alert(getLocaleMsg("registrationSuccess"));
+                    Lobibox.notify("success", {msg: getLocaleMsg("registrationSuccess")});
                     window.location.href = "login.html";
-                    return;
                 } else if (data.code == "00004") {
-                    alert(getLocaleMsg("passwordMustBeConsistent！"));
-                    return;
+                    Lobibox.notify("error", {msg: getLocaleMsg("passwordMustBeConsistent")});
                 } else if (data.code == "00005") {
-                    alert(getLocaleMsg("accountAlreadyExists"));
-                    return;
+                    Lobibox.notify("warning", {msg: getLocaleMsg("accountAlreadyExists")});
                 } else if (data.code == "00006") {
-                    alert(getLocaleMsg("nicknameAlreadyExists"));
-                    return;
+                    Lobibox.notify("warning", {msg: getLocaleMsg("nicknameAlreadyExists")});
                 }
             },
         });
