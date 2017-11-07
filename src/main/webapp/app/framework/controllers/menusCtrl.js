c6e15d5649beff1244c4b0b672d971307b87b47e
zpygithub <873333508@qq.com>
2017-11-07 20:02:44 +0800

    Delete menusCtrl.js

--- src/main/webapp/app/framework/controllers/menusCtrl.js
+++
@@ -1,9 +0,0 @@
-define([], function () {
-    var menusCtrl = ["$rootScope", "$scope", function ($rootScope, $scope) {
-//         $rootScope.menus = {
-//             "url": "app/framework/views/menus.html"
-//         };
-        console.log($rootScope.mainMenus);
-    }];
-    return menusCtrl;
-})  
