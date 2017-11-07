9ec9b3b3763d2fa3150549d02ed2a96e74793f7d
zpygithub <873333508@qq.com>
2017-11-07 20:04:16 +0800

    Update keyId.js

--- src/main/webapp/i18n/zh/keyId.js
+++ src/main/webapp/i18n/zh/keyId.js
@@ -3,6 +3,12 @@ define(function () {
         "term_errorForbidden_msg": "系统错误",
         "home_label": "首页",
         "user_label": "用户",
+        "user_register_label": "注册用户",
+        "user_statistics_label": "用户统计",
+        "goods_label": "商品",
+        "goods_classification_label": "商品分类",
+        "order_label": "订单",
+        "order_pending_label": "待处理订单",
     };
     return keyId;
 });
