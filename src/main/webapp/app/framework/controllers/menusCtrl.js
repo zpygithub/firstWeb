00e04171316659f668f93dc6da808613d8152b0b
zpygithub <873333508@qq.com>
2017-11-08 17:32:52 +0800

    Create menusCtrl.js

---
+++ src/main/webapp/app/framework/controllers/menusCtrl.js
@@ -0,0 +1,33 @@
+define([], function () {
+    "use strict";
+    var menusCtrl = ["$rootScope", "$scope", "$compile", "storage", function ($rootScope, $scope, $compile, storage) {
+        var personalInfo = $rootScope.personalInfo;
+        var i18n = $rootScope.i18n;
+
+        $scope.personalInfo = {
+            id: "personalInfoId",
+            text: personalInfo.account
+        };
+
+        $scope.modifyPersonalInfo = {
+            id: "modifyPersonalInfoId",
+            title: i18n.personal_info,
+            click: function () {
+                $("#modifyPersonalInfoId").modal({
+                    remote: "app/src/system/views/modifyPersonalInfo.html"
+                });
+            }
+        };
+
+        $scope.logout = {
+            id: "logoutId",
+            title: i18n.logout,
+            click: function () {
+                storage.flush();
+                window.location = "./logout";
+            }
+        };
+
+    }];
+    return menusCtrl;
+});
