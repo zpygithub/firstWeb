f0df6e1c85de3dd70f57ae816533e930b0371517
zpygithub <873333508@qq.com>
2017-11-10 15:44:03 +0800

    Update modifyPersonalInfoCtrl.js

--- src/main/webapp/app/src/system/controllers/modifyPersonalInfoCtrl.js
+++ src/main/webapp/app/src/system/controllers/modifyPersonalInfoCtrl.js
@@ -1,12 +1,12 @@
 define([], function () {
     "use strict";
-    var modifyPersonalInfoCtrl = ["$rootScope", "$scope", "$compile", function ($rootScope, $scope, $compile) {
+    var modifyAdminInfoCtrl = ["$rootScope", "$scope", "$compile", function ($rootScope, $scope, $compile) {
         $("#ok123").bind("click", function () {
             console.log(123);
         });
     }];
 
     var systemModule = angular.module("system.config");
-    systemModule.controller("modifyPersonalInfoCtrl", modifyPersonalInfoCtrl);
+    systemModule.controller("modifyAdminInfoCtrl", modifyAdminInfoCtrl);
     return systemModule;
 });
