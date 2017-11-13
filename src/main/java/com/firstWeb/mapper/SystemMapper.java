321ef3b02b75d4c3ee8e7d9bd51e36c350155fd8
zpygithub <873333508@qq.com>
2017-11-13 16:53:33 +0800

    Update SystemMapper.java

--- src/main/java/com/firstWeb/mapper/SystemMapper.java
+++ src/main/java/com/firstWeb/mapper/SystemMapper.java
@@ -1,15 +1,18 @@
 package com.firstWeb.mapper;
 
+import com.firstWeb.bean.model.Administrator;
 import com.firstWeb.bean.model.MainMenu;
+import com.firstWeb.bean.param.AdministratorParam;
 
 import java.util.List;
 import java.util.Map;
 
-/**
- * Created by zpy on 2017/2/13.
- */
 public interface SystemMapper {
 
     List<MainMenu> getMainMenus();
+
+    String modifyAdminInfo(AdministratorParam param);
+
+    Administrator getAdministratorById(long id);
 
 }
