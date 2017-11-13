d1de4ea544fdea069f9f2b0276db36e0f2079158
zpygithub <873333508@qq.com>
2017-11-13 16:53:06 +0800

    Update AdministratorMapper.java

--- src/main/java/com/firstWeb/mapper/AdministratorMapper.java
+++ src/main/java/com/firstWeb/mapper/AdministratorMapper.java
@@ -2,12 +2,9 @@ package com.firstWeb.mapper;
 
 import com.firstWeb.bean.model.Administrator;
 
-/**
- * Created by zpy on 2017/2/13.
- */
 public interface AdministratorMapper {
 
-    Administrator getAdministratorById(long id);
+    Administrator getAdministrator(long id);
 
     Administrator getAdministratorByAccount(String account);
 }
