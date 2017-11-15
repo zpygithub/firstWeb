564cc9e00055cb370971fc04f4a99e6ae16d7d17
zpygithub <873333508@qq.com>
2017-11-15 11:11:10 +0800

    Update keyId.js

--- src/main/webapp/i18n/zh/keyId.js
+++ src/main/webapp/i18n/zh/keyId.js
@@ -1,15 +1,11 @@
 define(function () {
     var keyId = {
+        //公共
         "term_errorForbidden_msg": "系统错误",
         "close": "关闭",
         "confirm": "确认",
-        "admin_info": "个人信息",
-        "modify_admin_info": "修改个人信息",
-        "admin_account": "账号",
-        "admin_nickname": "昵称",
-        "admin_email": "邮箱",
-        "admin_telephone": "电话",
-        "logout": "退出",
+        "operation_succeeded": "操作成功",
+        //菜单
         "home_label": "首页",
         "user_label": "用户",
         "user_register_label": "注册用户",
@@ -22,6 +18,17 @@ define(function () {
         "order_finished_label": "已完成订单",
         "system_label": "系统",
         "system_admin_label": "管理员管理",
+        //系统
+        "admin_info": "个人信息",
+        "modify_admin_info": "修改个人信息",
+        "admin_account": "账号",
+        "admin_nickname": "昵称",
+        "admin_email": "邮箱",
+        "admin_telephone": "电话",
+        "nicknameCanNotBeEmpty": "昵称不能为空。",
+        "nicknameFormatError": "昵称格式位2至8位任意字符。",
+        "nicknameAlreadyExists": "昵称已存在。",
+        "logout": "退出",
     };
     return keyId;
 });
