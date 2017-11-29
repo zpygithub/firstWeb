ad26185bd1c8a3bfd742f3acb7e120b096e1d677
zpygithub <873333508@qq.com>
2017-11-29 15:01:49 +0800

    Update LoginMapper.java

--- src/main/java/com/firstWeb/mapper/LoginMapper.java
+++ src/main/java/com/firstWeb/mapper/LoginMapper.java
@@ -1,12 +1,13 @@
 package com.firstWeb.mapper;
 
-/**
- * Created by zpy on 2017/2/13.
- */
+import com.firstWeb.bean.response.AdministratorInfo;
+
 public interface LoginMapper {
 
     String getSaltByAccount(String account);
 
     String getPasswdByAccount(String account);
+
+    AdministratorInfo getAdministratorByAccount(String account);
 
 }
