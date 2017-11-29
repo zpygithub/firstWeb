5391bbaac3772a080c5d076e179de72d7464634c
zpygithub <873333508@qq.com>
2017-11-29 10:17:58 +0800

    Update UserController.java

--- src/main/java/com/firstWeb/controller/UserController.java
+++ src/main/java/com/firstWeb/controller/UserController.java
@@ -1,6 +1,6 @@
 package com.firstWeb.controller;
 
-import com.firstWeb.bean.model.User;
+import com.firstWeb.bean.response.UserInfo;
 import com.firstWeb.service.UserService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -11,9 +11,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
-/**
- * Created by zpy on 2017/2/5.
- */
 @RestController
 @RequestMapping({"/user"})
 public class UserController {
@@ -26,14 +23,14 @@ public class UserController {
     @GetMapping(value = "/selectUserById/{id}")
     public String selectUserById(@PathVariable String id, HttpServletRequest request, HttpServletResponse response) {
         LOGGER.info("selectUserById: begin");
-        User user = userService.selectUserById(id);
+        UserInfo user = userService.selectUserById(id);
         return user.getEmail();
     }
 
     @RequestMapping(value = "/selectUserByConditions", method = RequestMethod.GET)
 //    @ResponseBody
-    public List<User> selectUserByConditions(String value, HttpServletRequest request, HttpServletResponse response) {
-        List<User> user = userService.selectUserByConditions("zxc");
+    public List<UserInfo> selectUserByConditions(String value, HttpServletRequest request, HttpServletResponse response) {
+        List<UserInfo> user = userService.selectUserByConditions("zxc");
         return user;
     }
 }
