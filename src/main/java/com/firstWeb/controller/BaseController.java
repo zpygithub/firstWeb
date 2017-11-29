20018fccc613bf207441ccaa82078aeb754e3105
zpygithub <873333508@qq.com>
2017-11-29 14:17:10 +0800

    Update BaseController.java

--- src/main/java/com/firstWeb/controller/BaseController.java
+++ src/main/java/com/firstWeb/controller/BaseController.java
@@ -1,13 +1,17 @@
 package com.firstWeb.controller;
 
 import com.firstWeb.bean.model.Token;
+import com.firstWeb.bean.response.PageInfo;
+import com.firstWeb.util.ParamValidateUtil;
+import org.springframework.util.StringUtils;
 
 import javax.servlet.http.HttpServletRequest;
 
-/**
- * Created by zpy on 2017/5/15.
- */
 public class BaseController {
+    private static final int MINCURRENTPAGE = 1;
+
+    private static final int MAXNUMFORSEARCH = 100;
+
     public Token getToken(HttpServletRequest request) {
         Token token = null;
         Object tokenObj = request.getAttribute("Attr_Token");
@@ -15,5 +19,21 @@ public class BaseController {
             token = (Token) tokenObj;
         }
         return token;
+    }
+
+    public PageInfo createPageInfo(String page, String size) {
+        PageInfo pageInfo = new PageInfo();
+        if ((!StringUtils.isEmpty(page) && ParamValidateUtil.isNumber(page)) && (!StringUtils.isEmpty(size) && ParamValidateUtil.isNumber(size))) {
+            int pageInt = Integer.parseInt(page);
+            int sizeInt = Integer.parseInt(size);
+            pageInfo.setCurrentPage(pageInt);
+            pageInfo.setPerPageRecords(sizeInt);
+            pageInfo.setFrom(pageInt, sizeInt);
+        } else {
+            pageInfo.setCurrentPage(MINCURRENTPAGE);
+            pageInfo.setPerPageRecords(MAXNUMFORSEARCH);
+            pageInfo.setFrom(MINCURRENTPAGE, MAXNUMFORSEARCH);
+        }
+        return pageInfo;
     }
 }
