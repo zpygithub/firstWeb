6470200302d66b700fff6ddd0bbb9dbcea092292
zpygithub <873333508@qq.com>
2017-11-29 10:17:12 +0800

    Create UserInfo.java

---
+++ src/main/java/com/firstWeb/bean/response/UserInfo.java
@@ -0,0 +1,58 @@
+package com.firstWeb.bean.response;
+
+public class UserInfo extends PageInfo {
+    private long id;
+    private String account;
+    private String nickname;
+    private String telephone;
+    private String email;
+    private int status;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+}
