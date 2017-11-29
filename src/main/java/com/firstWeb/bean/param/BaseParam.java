9564f65049d2cafa7f47482cc64d2a0a1e6ccd99
zpygithub <873333508@qq.com>
2017-11-29 14:16:58 +0800

    Create BaseParam.java

---
+++ src/main/java/com/firstWeb/bean/param/BaseParam.java
@@ -0,0 +1,15 @@
+package com.firstWeb.bean.param;
+
+import com.firstWeb.bean.response.PageInfo;
+
+public class BaseParam {
+    private PageInfo pageInfo;
+
+    public PageInfo getPageInfo() {
+        return pageInfo;
+    }
+
+    public void setPageInfo(PageInfo pageInfo) {
+        this.pageInfo = pageInfo;
+    }
+}
