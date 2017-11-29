15c25d4a20146422e667af408dc902ea894e48cb
zpygithub <873333508@qq.com>
2017-11-29 14:18:47 +0800

    Update PageInfo.java

--- src/main/java/com/firstWeb/bean/response/PageInfo.java
+++ src/main/java/com/firstWeb/bean/response/PageInfo.java
@@ -1,9 +1,26 @@
 package com.firstWeb.bean.response;
 
 public class PageInfo {
-
+    private int currentPage;
+    private int perPageRecords;
     private int totalRecords;
     private int from;
+
+    public int getCurrentPage() {
+        return currentPage;
+    }
+
+    public void setCurrentPage(int currentPage) {
+        this.currentPage = currentPage;
+    }
+
+    public int getPerPageRecords() {
+        return perPageRecords;
+    }
+
+    public void setPerPageRecords(int perPageRecords) {
+        this.perPageRecords = perPageRecords;
+    }
 
     public int getTotalRecords() {
         return totalRecords;
@@ -17,7 +34,7 @@ public class PageInfo {
         return from;
     }
 
-    public void setFrom(int from) {
-        this.from = from;
+    public void setFrom(int currentPage, int perPageRecord) {
+        this.from = perPageRecord * (currentPage - 1);
     }
 }
