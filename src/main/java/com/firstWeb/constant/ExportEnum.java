c7c21be2e0f9b5c80ee95c20db25b619db977a4f
zpygithub <873333508@qq.com>
2017-12-21 19:48:54 +0800

    Create ExportEnum.java

---
+++ src/main/java/com/firstWeb/constant/ExportEnum.java
@@ -0,0 +1,33 @@
+package com.firstWeb.constant;
+
+public enum ExportEnum {
+    adminList("管理员列表"),
+
+    id("id"),
+
+    account("账号"),
+
+    nickname("昵称"),
+
+    email("邮箱"),
+
+    telephone("电话"),
+
+    createTime("创建时间"),
+
+    status("状态"),
+
+    normal("正常"),
+
+    freeze("冻结");
+
+    private String value;
+
+    ExportEnum(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
