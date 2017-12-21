ecaf165cde6d3f2e7a2c8dd549f76205ef936bbb
zpygithub <873333508@qq.com>
2017-12-21 19:44:00 +0800

    Update DateUtil.java

--- src/main/java/com/firstWeb/util/DateUtil.java
+++ src/main/java/com/firstWeb/util/DateUtil.java
@@ -1,13 +1,24 @@
 package com.firstWeb.util;
 
+import java.util.Calendar;
 import java.util.Date;
 
 public class DateUtil {
     public static Date getUTCDate() {
-        java.util.Calendar cal = java.util.Calendar.getInstance();
-        int zoneOffset = cal.get(java.util.Calendar.ZONE_OFFSET);
-        int dstOffset = cal.get(java.util.Calendar.DST_OFFSET);
-        cal.add(java.util.Calendar.MILLISECOND, -(zoneOffset + dstOffset));
+        Calendar cal = Calendar.getInstance();
+        int zoneOffset = cal.get(Calendar.ZONE_OFFSET);
+        int dstOffset = cal.get(Calendar.DST_OFFSET);
+        cal.add(Calendar.MILLISECOND, -(zoneOffset + dstOffset));
         return new Date(cal.getTimeInMillis());
+    }
+
+    public static Date getLocalTimeFromUTC(Date utcTime)
+    {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(utcTime);
+        int zoneOffset = cal.get(Calendar.ZONE_OFFSET);
+        int dstOffset = cal.get(Calendar.DST_OFFSET);
+        cal.add(Calendar.MILLISECOND, +(zoneOffset + dstOffset));
+        return cal.getTime();
     }
 }
