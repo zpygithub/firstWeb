86d57bdc0ff16fc61ad3913a0f1081c0dc481396
zpygithub <873333508@qq.com>
2017-12-22 16:49:51 +0800

    Create ExportTaskInfo.java

---
+++ src/main/java/com/firstWeb/bean/response/ExportTaskInfo.java
@@ -0,0 +1,85 @@
+package com.firstWeb.bean.response;
+
+import java.util.Date;
+
+public class ExportTaskInfo {
+    private long id;
+
+    private String taskName;
+
+    private String fileName;
+
+    private String downloadURL;
+
+    private long creatorId;
+
+    private Date createTime;
+
+    private int status;
+
+    private String remark;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getDownloadURL() {
+        return downloadURL;
+    }
+
+    public void setDownloadURL(String downloadURL) {
+        this.downloadURL = downloadURL;
+    }
+
+    public long getCreatorId() {
+        return creatorId;
+    }
+
+    public void setCreatorId(long creatorId) {
+        this.creatorId = creatorId;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+}
