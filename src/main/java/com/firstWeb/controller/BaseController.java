63e87005daebf612e1f8cf2af13550c702207499
zpygithub <873333508@qq.com>
2017-12-26 23:54:52 +0800

    Update BaseController.java

--- src/main/java/com/firstWeb/controller/BaseController.java
+++ src/main/java/com/firstWeb/controller/BaseController.java
@@ -3,11 +3,17 @@ package com.firstWeb.controller;
 import com.firstWeb.bean.model.Token;
 import com.firstWeb.bean.response.PageInfo;
 import com.firstWeb.util.ParamValidateUtil;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.util.StringUtils;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
 
 public class BaseController {
+    private static final Logger LOGGER = LogManager.getLogger(BaseController.class);
+
     private static final int MINCURRENTPAGE = 1;
 
     private static final int MAXNUMFORSEARCH = 100;
@@ -35,5 +41,57 @@ public class BaseController {
             pageInfo.setFrom(MINCURRENTPAGE, MAXNUMFORSEARCH);
         }
         return pageInfo;
+    }
+
+    protected void downloadToPage(String path, HttpServletResponse response) {
+        BufferedInputStream in = null;
+        BufferedOutputStream out = null;
+
+        File file = new File(path);
+        String filename = file.getName();
+        // 以流的形式下载文件。
+//            InputStream fis = new BufferedInputStream(new FileInputStream(path));
+//            byte[] buffer = new byte[fis.available()];
+//            fis.read(buffer);
+//            fis.close();
+        // 清空response
+        response.reset();
+        response.setHeader("Content-Disposition", "attachment;filename=" + filename);
+        response.setHeader("Content-Length", String.valueOf(file.length()));
+        response.setContentType("application/octet-stream");
+        try {
+            in = new BufferedInputStream(new FileInputStream(file));
+            out = new BufferedOutputStream(response.getOutputStream());
+            byte[] b = new byte[4096];
+            int count = 0;
+            while ((count = in.read(b, 0, b.length)) != -1) {
+                out.write(b, 0, count);
+            }
+            out.flush();
+        } catch (IOException e) {
+            LOGGER.error(e);
+        } finally {
+            if (null != in) {
+                closeStream(in, out);
+            }
+        }
+    }
+
+    private static void closeStream(InputStream in, OutputStream out) {
+        try {
+            if (out != null) {
+                out.close();
+            }
+        } catch (IOException e) {
+            LOGGER.error("bufferedOutputStream close failed" + e);
+        } finally {
+            try {
+                if (in != null) {
+                    in.close();
+                }
+            } catch (IOException e) {
+                LOGGER.error("bufferedInputStream close failed" + e);
+            }
+        }
     }
 }
