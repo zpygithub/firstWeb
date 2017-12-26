46609de6b6547a011a490ae7b56bbd20fd0a6acb
zpygithub <873333508@qq.com>
2017-12-26 23:52:16 +0800

    Update TaskService.java

--- src/main/java/com/firstWeb/service/TaskService.java
+++ src/main/java/com/firstWeb/service/TaskService.java
@@ -7,6 +7,7 @@ import com.firstWeb.constant.ResultCode;
 import com.firstWeb.exception.CommonException;
 import com.firstWeb.mapper.TaskMapper;
 import com.firstWeb.util.DateUtil;
+import com.firstWeb.util.PropertiesUtil;
 import com.firstWeb.util.ZipUtil;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -17,7 +18,10 @@ import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.security.SecureRandom;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -28,11 +32,15 @@ public class TaskService {
 
     private static final Logger LOGGER = LogManager.getLogger(TaskService.class);
 
+    private static final String ZIP = ".zip";
+
+    private static String EXPORTFILEPATH = PropertiesUtil.getValue("export.file.path");
+
     public ExportTaskParam addExportTask(ExportTaskParam params) throws CommonException {
         if (ExportStatusEnum.EXPORTISRUNNING.getValue() != this.getRunningTaskQuantities(params.getCreatorId())) {
             throw new CommonException(ResultCode.EXPORTTASKISRUNNING, "export task is running");
         }
-        params.setFileName(params.getFileName() + ".zip");
+        params.setFileName(params.getFileName() + ZIP);
         params.setCreateTime(DateUtil.getUTCDate());
         params.setStatus(ExportStatusEnum.EXPORTISRUNNING.getValue());
         params.setRemark(this.getSixRandomPasswd());
@@ -66,53 +74,35 @@ public class TaskService {
         taskMapper.updateExportTask(params);
     }
 
-    public void updateExportTask(List<String> downloadPaths, ExportTaskParam params) {
+    public String updateExportTask(String filePath, ExportTaskParam params) {
+        String downloadURL = "";
         try {
-            if (!downloadPaths.isEmpty()) {
-                String downloadURL = this.encryptCompressFile(downloadPaths, params);
-                params.setDownloadURL(downloadURL);
-                params.setStatus(ExportStatusEnum.SUCCESS.getValue());
-            } else {
-                params.setStatus(ExportStatusEnum.EXPORTNODATA.getValue());
-            }
+            downloadURL = this.encryptCompressFile(filePath, params);
+            params.setDownloadURL(downloadURL);
+            params.setStatus(ExportStatusEnum.SUCCESS.getValue());
             this.updateExportTask(params);
         } catch (Exception e) {
             LOGGER.error(e);
         }
+        return downloadURL;
     }
 
     /**
      * 加密压缩
      *
-     * @param downloadPaths
+     * @param filePath
      * @param params
      * @return
      * @throws Exception
      */
-    private String encryptCompressFile(List<String> downloadPaths, ExportTaskParam params) throws Exception {
-//        String dest_file_name = params.getFileName();
-//        String relativePath = "temp" + File.separator + getDateStoragePath()
-//                + (new StringBuffer(UUID.randomUUID().toString())) + File.separator + dest_file_name;
-//        String compressPath = ATTACHMENT_STORAGE_PATH + relativePath;
-//
-//        File dest = FileUtils.getFile(compressPath);
-//        if (null != dest) {
-//            if (null != dest.getParentFile() && !dest.getParentFile().exists()) {
-//                if (!dest.getParentFile().mkdirs()) {
-//                    throw new Exception("mkdirs error.");
-//                }
-//            }
-//        }
-//
-//        ZipUtil zc = new ZipUtil(compressPath);
-//        zc.encryptCompress(downloadPaths, compressPath, params.getRemark());
-//
-//        for (String path : downloadPaths) {
-//            FileUtil.deletefile(new File(PathUtils.FilePathFormat(path)).getParentFile().getPath());
-//        }
-//
-//        return relativePath;
-        return null;
+    private String encryptCompressFile(String filePath, ExportTaskParam params) throws Exception {
+        String fileName = params.getFileName();
+        String compressPath = EXPORTFILEPATH + fileName;
+        ZipUtil zipUtil = new ZipUtil(compressPath);
+        zipUtil.encryptCompress(filePath, compressPath, params.getRemark());
+        File file = new File(filePath);
+        file.delete();
+        return compressPath;
     }
 
 }
