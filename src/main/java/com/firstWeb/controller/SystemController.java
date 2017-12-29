7a6dfcaee2f2e45a32ff1beab2ff67f9e8d3d1f3
zpygithub <873333508@qq.com>
2017-12-29 17:30:53 +0800

    Update SystemController.java

--- src/main/java/com/firstWeb/controller/SystemController.java
+++ src/main/java/com/firstWeb/controller/SystemController.java
@@ -1,207 +1,232 @@
-package com.firstWeb.controller;
+define(["i18n/keyId", "bootstrap-table", "app/services/commonService"], function (i18n, bootstrapTable, CommonService) {
+    "use strict";
+    var adminListCtrl = ["$rootScope", "$scope", "$compile", function ($rootScope, $scope, $compile) {
+        var commonService = new CommonService($scope);
 
-import com.firstWeb.bean.model.ExportTask;
-import com.firstWeb.bean.model.MainMenu;
-import com.firstWeb.bean.param.AdministratorParam;
-import com.firstWeb.bean.param.ExportTaskParam;
-import com.firstWeb.bean.request.AdministratorReqModel;
-import com.firstWeb.bean.response.AdministratorInfo;
-import com.firstWeb.bean.response.ExportTaskInfo;
-import com.firstWeb.common.CollectionResult;
-import com.firstWeb.common.ResultEntity;
-import com.firstWeb.constant.ExportEnum;
-import com.firstWeb.constant.ResultCode;
-import com.firstWeb.exception.CommonException;
-import com.firstWeb.service.RegisterService;
-import com.firstWeb.service.SystemService;
-import com.firstWeb.service.TaskService;
-import com.firstWeb.util.BasicDateValidateUtil;
-import com.firstWeb.util.ParamValidateUtil;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+        $scope.account = {
+            id: "accountId",
+            label: i18n.account,
+            value: ""
+        };
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.List;
+        $scope.nickname = {
+            id: "nicknameId",
+            label: i18n.nickname,
+            value: ""
+        };
 
-@RestController
-@RequestMapping({"/system"})
-public class SystemController extends BaseController {
+        $scope.email = {
+            id: "emailId",
+            label: i18n.email,
+            value: ""
+        };
 
-    @Autowired
-    private SystemService systemService;
+        $scope.telephone = {
+            id: "telephoneId",
+            label: i18n.telephone,
+            value: ""
+        };
 
-    @Autowired
-    private RegisterService registerService;
+        $scope.query = {
+            id: "queryId",
+            label: i18n.query
+        };
 
-    @Autowired
-    private TaskService taskService;
+        $scope.reset = {
+            id: "resetId",
+            label: i18n.reset
+        };
 
-    private static final Logger LOGGER = LogManager.getLogger(SystemController.class);
+        $scope.export = {
+            id: "exportId",
+            label: i18n.export
+        };
 
-    /**
-     * 获取菜单
-     *
-     * @param request
-     * @param response
-     * @return
-     */
-    @GetMapping(value = "/getMainMenus")
-    public ResultEntity<List<MainMenu>> getMainMenus(HttpServletRequest request, HttpServletResponse response) {
-        LOGGER.info("getMainMenus: begin");
-        ResultEntity<List<MainMenu>> result = new ResultEntity<>();
-        result.setCode(ResultCode.FAIL);
-//        Token token = getToken(request);
-        List<MainMenu> list = systemService.getMainMenus();
-        result.setValue(list);
-        result.setCode(ResultCode.SUCCESS);
-        LOGGER.info("getMainMenus: end");
-        return result;
-    }
+        $("#query").bind("click", function () {
+            $scope.account.value = $("#account").val().trim();
+            $scope.nickname.value = $("#nickname").val().trim();
+            $scope.email.value = $("#email").val().trim();
+            $scope.telephone.value = $("#telephone").val().trim();
+            $("#adminList").bootstrapTable('refresh', {pageNumber: 1});
+        });
 
-    /**
-     * 获取管理员列表
-     *
-     * @param request
-     * @param response
-     * @return
-     */
-    @GetMapping(value = "/getAdminListOnCondition")
-    public ResultEntity<CollectionResult<AdministratorInfo>> getAdminListOnCondition(AdministratorReqModel model, HttpServletRequest request, HttpServletResponse response) {
-        LOGGER.info("getAdminListOnCondition: begin");
-        ResultEntity<CollectionResult<AdministratorInfo>> result = new ResultEntity<>();
-        result.setCode(ResultCode.FAIL);
-//        Token token = getToken(request);
-        AdministratorParam params = new AdministratorParam();
-        params.setAccount(ParamValidateUtil.escapeSpecialCharacter(model.getAccount()));
-        params.setNickname(ParamValidateUtil.escapeSpecialCharacter(model.getNickname()));
-        params.setEmail(ParamValidateUtil.escapeSpecialCharacter(model.getEmail()));
-        params.setTelephone(ParamValidateUtil.escapeSpecialCharacter(model.getTelephone()));
-        params.setPageInfo(createPageInfo(model.getPage(), model.getSize()));
-        CollectionResult<AdministratorInfo> list = systemService.getAdminListOnCondition(params);
-        result.setValue(list);
-        result.setCode(ResultCode.SUCCESS);
-        LOGGER.info("getAdminListOnCondition: end");
-        return result;
-    }
+        $("#reset").bind("click", function () {
+            $scope.account.value = "";
+            $scope.nickname.value = "";
+            $scope.email.value = "";
+            $scope.telephone.value = "";
+            $("#adminList").bootstrapTable('refresh', {pageNumber: 1});
+        });
 
-    /**
-     * 修改管理员
-     *
-     * @param request
-     * @param response
-     * @return
-     */
-    @PostMapping(value = "/modifyAdminInfo")
-    public ResultEntity<String> modifyAdminInfo(AdministratorReqModel model, HttpServletRequest request, HttpServletResponse response) throws CommonException {
-        LOGGER.info("modifyAdminInfo: begin");
-        ResultEntity<String> result = new ResultEntity<>();
-        result.setCode(ResultCode.FAIL);
-//        Token token = getToken(request);
+        $("#export").bind("click", function () {
+            var options = {
+                account: $("#account").val().trim(),
+                nickname: $("#nickname").val().trim(),
+                email: $("#email").val().trim(),
+                telephone: $("#telephone").val().trim()
+            };
+            $scope.operate.export(options);
+        });
 
-        BasicDateValidateUtil.validateIsEmpty(model.getNickname());
-        BasicDateValidateUtil.validateIsNickname(model.getNickname());
+        $scope.operate = {
+            "query": function () {
+                $("#adminList").bootstrapTable({
+                    url: "system/getAdminListOnCondition",         //请求后台的URL（*）
+                    method: "get",                      //请求方式（*）
+                    toolbar: "#toolbar",                //工具按钮用哪个容器
+                    striped: false,                      //是否显示行间隔色
+                    pagination: true,                   //是否显示分页（*）
+                    sortable: true,                    //是否启用排序
+                    sortOrder: "asc",                   //排序方式
+                    queryParamsType: 'limit',
+                    queryParams: queryParams,           //传递参数（*）
+                    sidePagination: "server",           //分页方式：client客户端分页，server服务端分页（*）
+                    dataType: "json",
+                    pageNumber: 1,                      //初始化加载第一页，默认第一页
+                    pageSize: 5,                       //每页的记录行数（*）
+                    pageList: [10, 25, 50, 100],        //可供选择的每页的行数（*）
+                    minimumCountColumns: 2,             //最少允许的列数
+                    clickToSelect: true,                //是否启用点击选中行
+                    uniqueId: "id",                     //每一行的唯一标识，一般为主键列
+                    // paginationPreText: "上一页",
+                    // paginationNextText: "下一页",
+                    responseHandler: getAdminListOnCondition,
+                    columns: [
+                        {
+                            field: "id",
+                            title: "id",
+                            align: "center",
+                            valign: "middle",
+                            sortable: true
+                        }, {
+                            field: "account",
+                            title: i18n.account,
+                            align: "center",
+                            valign: "middle",
+                            sortable: true
+                        }, {
+                            field: "nickname",
+                            title: i18n.nickname,
+                            align: "center",
+                            valign: "middle"
+                        }, {
+                            field: "email",
+                            title: i18n.email,
+                            align: "center",
+                            valign: "middle"
+                        }, {
+                            field: "telephone",
+                            title: i18n.telephone,
+                            align: "center",
+                            valign: "middle"
+                        }, {
+                            field: "createTime",
+                            title: i18n.createTime,
+                            align: "center",
+                            valign: "left",
+                            formatter: function (value) {
+                                value = commonService.getFormatTime(value);
+                                return value;
+                            }
+                        }, {
+                            field: "status",
+                            title: i18n.status,
+                            align: "center",
+                            valign: "middle",
+                            formatter: function (status) {
+                                if (status == 0) {
+                                    return i18n.normal;
+                                } else {
+                                    return i18n.freeze;
+                                }
+                            }
+                        }]
+                });
+            },
+            "export": function (options) {
+                $.ajax({
+                    type: 'post',
+                    url: 'system/exportAdminList/',
+                    dataType: 'json',
+                    async: false,
+                    data: options
+                }).done(function (data) {
+                    if (data.code === "00000") {
+                        Lobibox.notify("success", {msg: i18n.operation_succeeded});
+                        console.log(data.value);
+                        if (data.value.downloadUrl && data.value.remark) {
 
-        if (!registerService.checkNickname(model.getNickname()) && !systemService.checkOwnNickname(model.getNickname(), model.getId())) {
-            result.setCode(ResultCode.NICKNAMEISEXISTENCE);
-            return result;
+                            $("#compressPw").val(data.value.compressPw);
+                            openPostWindow('system/downLoadToPage', data.value.downloadUrl);
+                        }
+                    } else if (data.code === "00012") {
+                        Lobibox.notify("warning", {msg: i18n.export_task_is_running});
+                    } else {
+                        Lobibox.notify("error", {msg: i18n.operation_failed});
+                    }
+                });
+            }
         }
 
-        AdministratorParam param = new AdministratorParam();
-        param.setId(model.getId());
-        param.setNickname(model.getNickname());
-        param.setEmail(model.getEmail());
-        param.setTelephone(model.getTelephone());
-        systemService.modifyAdminInfo(param);
+        var openPostWindow = function (url, attachAddress) {
+            var tempForm = document.createElement("form");
+            tempForm.id = "tempForm1";
+            tempForm.method = "post";
+            tempForm.action = url;
 
-        result.setCode(ResultCode.SUCCESS);
-        LOGGER.info("modifyAdminInfo: end");
-        return result;
-    }
+            var hideInput1 = document.createElement("input");
+            hideInput1.type = "hidden";
+            hideInput1.name = "attachAddress";
+            hideInput1.value = attachAddress;
 
-    /**
-     * 获取管理员信息
-     *
-     * @param request
-     * @param response
-     * @return
-     * @throws CommonException
-     */
-    @GetMapping(value = "/getAdministratorById/{id}")
-    public ResultEntity<AdministratorInfo> getAdministratorById(@PathVariable String id, HttpServletRequest request, HttpServletResponse response) throws CommonException {
-        LOGGER.info("getAdministratorById: begin");
-        ResultEntity<AdministratorInfo> result = new ResultEntity<>();
-        result.setCode(ResultCode.FAIL);
-        AdministratorInfo administrator = systemService.getAdministratorById(Long.valueOf(id));
-        if (null == administrator) {
-            throw new CommonException(ResultCode.ACCOUNTISNOTEXISTENCE, "the administrator not exist.");
+            tempForm.appendChild(hideInput1);
+            if (document.all) {
+                tempForm.attachEvent("onsubmit", function () {
+                });
+            } else {
+                var subObj = tempForm.addEventListener("submit", function () {
+                }, false);
+            }
+            document.body.appendChild(tempForm);
+            tempForm.submit();
+            document.body.removeChild(tempForm);
         }
-        result.setValue(administrator);
-        result.setCode(ResultCode.SUCCESS);
-        LOGGER.info("getAdministratorById: end");
-        return result;
-    }
 
-    /**
-     * 获取管理员信息
-     *
-     * @param request
-     * @param response
-     * @return
-     * @throws CommonException
-     */
-    @GetMapping(value = "/getAdministrator")
-    public ResultEntity<AdministratorInfo> getAdministrator(HttpServletRequest request, HttpServletResponse response) throws CommonException {
-        LOGGER.info("getAdministrator: begin");
-        ResultEntity<AdministratorInfo> result = new ResultEntity<>();
-        result.setCode(ResultCode.FAIL);
-//        Token token = getToken(request);
-//        if (null != token) {
-//            long id = token.getId();
-        AdministratorInfo administrator = systemService.getAdministratorById(1);
-        if (null == administrator) {
-            throw new CommonException(ResultCode.ACCOUNTISNOTEXISTENCE, "the administrator not exist.");
+
+        function queryParams() {
+            var options = {
+                page: this.pageNumber,
+                size: this.pageSize,
+                account: $scope.account.value,
+                nickname: $scope.nickname.value,
+                email: $scope.email.value,
+                telephone: $scope.telephone.value
+            };
+            return options;
         }
-        result.setValue(administrator);
-        result.setCode(ResultCode.SUCCESS);
-//        }
-        LOGGER.info("getAdministrator: end");
-        return result;
-    }
 
-    /**
-     * 导出管理员列表
-     *
-     * @param model
-     * @param request
-     * @param response
-     * @return
-     */
-    @PostMapping(value = "/exportAdminList")
-    public ResultEntity<String> exportAdminList(AdministratorReqModel model, HttpServletRequest request, HttpServletResponse response) throws IOException, CommonException {
-        LOGGER.info("exportAdminList: begin");
-        ResultEntity<String> result = new ResultEntity<>();
-        result.setCode(ResultCode.FAIL);
-//        Token token = getToken(request);
-//        long id = token.getId();
-        ExportTaskParam exportTaskparams = new ExportTaskParam();
-        exportTaskparams.setCreatorId(1);
+        function getAdminListOnCondition(res) {
+            if (res) {
+                return {
+                    "rows": res.value.list,
+                    "total": res.value.pageInfo.totalRecords
+                };
+            } else {
+                return {
+                    "rows": [],
+                    "total": 0
+                };
+            }
+        }
 
-        AdministratorParam params = new AdministratorParam();
-        params.setAccount(ParamValidateUtil.escapeSpecialCharacter(model.getAccount()));
-        params.setNickname(ParamValidateUtil.escapeSpecialCharacter(model.getNickname()));
-        params.setEmail(ParamValidateUtil.escapeSpecialCharacter(model.getEmail()));
-        params.setTelephone(ParamValidateUtil.escapeSpecialCharacter(model.getTelephone()));
-        String downloadUrl = systemService.exportAdminList(params, exportTaskparams);
+        function init() {
+            $scope.operate.query();
+        }
 
-//        this.downloadToPage(downloadUrl, response);
+        init();
+    }];
 
-        result.setCode(ResultCode.SUCCESS);
-        LOGGER.info("exportAdminList: end");
-        return result;
-    }
-
-}
+    var systemModule = angular.module("system.config");
+    systemModule.controller("system.adminList", adminListCtrl);
+    return systemModule;
+});
