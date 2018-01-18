46dcec5f0a43adde1d0954de4ba5d81dee78a354
zpygithub <873333508@qq.com>
2018-01-18 17:31:15 +0800

    Update keyId.js

--- src/main/webapp/i18n/zh/keyId.js
+++ src/main/webapp/i18n/zh/keyId.js
@@ -1,7 +1,7 @@
 define(function () {
     var keyId = {
         //公共
-        "term_errorForbidden_msg": "系统错误",
+        "term_errorForbidden_msg": "系统错误！",
         "close": "关闭",
         "confirm": "确认",
         "account": "账号",
@@ -25,11 +25,11 @@ define(function () {
         "query": "查询",
         "reset": "重置",
         "export": "导出",
-        "operation_succeeded": "操作成功",
-        "operation_failed": "操作失败",
+        "operation_succeeded": "操作成功！",
+        "operation_failed": "操作失败！",
         //导出
-        "export_task_is_running": "已有导出任务正在进行，请稍后重试",
-        "export_no_data": "导出数据为空",
+        "export_task_is_running": "已有导出任务正在进行，请稍后重试。",
+        "export_no_data": "导出数据为空！",
         "unzip_password": "解压密码：",
         //菜单
         "home_label": "首页",
@@ -48,10 +48,12 @@ define(function () {
         "system_admin_list": "管理员列表",
         "admin_info": "个人信息",
         "modify_admin_info": "修改个人信息",
-        "username_cannot_be_empty": "用户名不能为空。",
+        "username_cannot_be_empty": "用户名不能为空！",
         "username_format_error": "用户名格式位2至8位任意字符。",
         "username_already_exists": "用户名已存在。",
         "logout": "退出",
+        "freeze_admin": "确认冻结该管理员吗？",
+        "recover_admin": "确认恢复该管理员吗？",
         //用户
         "user_register_list": "注册用户列表",
     };
