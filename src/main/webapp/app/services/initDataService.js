0e328a91b8b0ef8c2507945ed935f712c04133a0
zpygithub <873333508@qq.com>
2018-01-23 20:04:26 +0800

    Create initDataService.js

---
+++ src/main/webapp/app/services/initDataService.js
@@ -0,0 +1,84 @@
+define(['i18n/keyId'], function (i18n) {
+    "use strict";
+    var initDataService = function () {
+        this.initDistrict = function ($scope) {
+            $scope.province = {
+                id: "provinceId",
+                value: "",
+                disabled: false,
+                selectedOptions: "",
+                select: function () {
+                    $scope.city.value = "";
+                    $scope.district.value = "";
+                    $scope.city.disabled = false;
+                    $scope.district.disabled = false;
+                    var selectedValue = $scope.province.value;
+                    if (selectedValue) {
+                        initDistrictData($scope, selectedValue);
+                    }
+                }
+            };
+            $scope.city = {
+                id: "cityId",
+                value: "",
+                disabled: true,
+                selectedOptions: "",
+                select: function () {
+                    var selectedValue = $scope.city.value;
+                    if (selectedValue) {
+                        initDistrictData($scope, selectedValue);
+                    }
+                }
+            }
+            $scope.district = {
+                id: "districtId",
+                label: i18n.district,
+                value: "",
+                disabled: true,
+                selectedOptions: ""
+            }
+            initDistrictData($scope, "init");
+        };
+
+        function initDistrictData($scope, code) {
+            $.ajax({
+                type: "get",
+                url: "system/getDistricts/" + code,
+                dataType: "json",
+                async: false
+            }).done(function (data) {
+                var valuesArr = [];
+                if (data.code === "00000") {
+                    if (data.value && data.value.length > 0) {
+                        data.value.forEach(function (item) {
+                            var obj = {
+                                value: item.id,
+                                text: item.name
+                            };
+                            valuesArr.push(obj);
+                        });
+                        if (code == "init") {
+                            $scope.province.selectedOptions = valuesArr;
+                        } else if (code.indexOf("0000") > -1) {
+                            $scope.city.disabled = false;
+                            $scope.city.selectedOptions = valuesArr;
+                        } else {
+                            $scope.district.disabled = false;
+                            $scope.district.selectedOptions = valuesArr;
+                        }
+                    } else {
+                        $scope.district.disabled = true;
+                        if (!$scope.city.value) {
+                            $scope.city.selectedOptions = "";
+                            $scope.city.disabled = true;
+                        } else {
+                            $scope.district.selectedOptions = "";
+                        }
+                    }
+                }
+            });
+        };
+
+    };
+    return initDataService;
+});
