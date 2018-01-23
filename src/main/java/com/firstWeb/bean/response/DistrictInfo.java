d2cb33dfed3b438f1ecbbe48aeb8dd98aaf30bc7
zpygithub <873333508@qq.com>
2018-01-23 20:05:11 +0800

    Create DistrictInfo.java

---
+++ src/main/java/com/firstWeb/bean/response/DistrictInfo.java
@@ -0,0 +1,31 @@
+package com.firstWeb.bean.response;
+
+public class DistrictInfo {
+    private long id;
+    private long parentId;
+    private String name;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(long parentId) {
+        this.parentId = parentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
